3b4aa3a6c603de7c9b24af8f639f4edbc767c716
Paul Park <paulpark330@hotmail.com>
2023-03-19 01:51:12 -0700

    style fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,11 @@
 import Header from "@/components/Header/Header";
-import Image from "next/image";
 import styles from "./page.module.scss";
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <Header title="Home" />
-      <div className={styles.content}>
-        {/* <Image width={400} height={400} src="/assets/clerkie-icon.svg" alt="logo" /> */}
-      </div>
+      <div className={styles.content}></div>
     </main>
   );
 };
